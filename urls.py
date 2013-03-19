9beae7789e4fd78d8cf653ee6eeeb4f49d66c389
schitic <chiticstefan@gmail.com>
2013-03-19 09:32:33 +0100

    Add link to api

--- urls.py
+++ urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     (r'^api/*',include('api.urls')),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'index.html'}),
+    (r'^download/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': '/download'}),
 )
